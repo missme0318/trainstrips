4624e3648530e15dff29c445acf0503e33005e67
missme0318 <your_email>
2022-07-11 07:48:07 +0800

    test

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support.ui import Select
 
 from googlemap import input_wanted
 from searchtrips import search_trips
-#from bookingtrains import *
+from booking import booking_train, solveRecaptha
 
 
 app = Flask(__name__)
