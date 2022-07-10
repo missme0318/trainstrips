5ec0c1b64ba28d0a81b6ee796adbe5f767276f52
missme0318 <your_email>
2022-07-11 05:22:55 +0800

    test

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 
+from googlemap import input_wanted
+
 
 app = Flask(__name__)
 
@@ -18,47 +20,32 @@ line_bot_api = LineBotApi('cBwUwzLyXqDhFhdsG/cglur32QRiBgbAi/3Xq3eby34MUg1zcQi2Y
 handler = WebhookHandler('79a8d7930208c29ff1601c21c2683c37')
 
 
-def input_wanted(search):
-    address, limittime = [], []
-    #process = 'start'
+# def input_wanted(search):
+#     address, limittime = [], []
+  
+#     chromeOption = webdriver.ChromeOptions()
+#     chromeOption.add_argument("--lang=zh-CN.UTF8")
+#     chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
+#     driver = webdriver.Chrome(chrome_options=chromeOption)
     
-    # chromeOption = webdriver.ChromeOptions()
+#     # driver.set_window_size(1024, 960)
 
-    # chromeOption.add_argument("start-maximized")
-    # chromeOption.add_argument('--headless')
-    # chromeOption.add_argument('--no-sandbox')
-    # chromeOption.add_argument('--disable-dev-shm-usage')
-    # driver = webdriver.Chrome(chrome_options=chromeOption)
-
-    chromeOption = webdriver.ChromeOptions()
-    chromeOption.add_argument("--lang=zh-CN.UTF8")
-    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    driver = webdriver.Chrome(chrome_options=chromeOption)
-        
-        # driver = webdriver.Chrome()
-        # driver.set_window_size(1024, 960)
-
-    # driver = webdriver.Chrome(service=s, options=chromeOptions)
-    driver.get(f'https://www.google.com.tw/maps/search/{search}/@23.546162,120.6402133,8z?hl=zh-TW/data=!4m4!2m3!5m1!2e1!6e5')
+#     driver.get(f'https://www.google.com.tw/maps/search/{search}/@23.546162,120.6402133,8z?hl=zh-TW/data=!4m4!2m3!5m1!2e1!6e5')
     
-        
-        # title = driver.find_element(By.ID, 'searchboxinput')
-        # title.send_keys('高雄美食')
 
-    #driver.maximize_window()
+#     #driver.maximize_window()
 
-    driver.implicitly_wait(2)
+#     driver.implicitly_wait(2)
 
-        # 1st info
-    operation = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]')
+#     operation = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]')
     
-    name_type = operation.find_elements(By.CLASS_NAME, 'Nv2PK')
-    name = [i.text.split('\n')[0] for i in name_type]
+#     name_type = operation.find_elements(By.CLASS_NAME, 'Nv2PK')
+#     name = [i.text.split('\n')[0] for i in name_type]
 
 
-    driver.quit()
+#     driver.quit()
 
-    return name[0]
+#     return name[0]
 
 def testing(name):
     return name
---
+++ googlemap.py
@@ -0,0 +1,35 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+
+
+def input_wanted(search):
+    address, limittime = [], []
+  
+    chromeOption = webdriver.ChromeOptions()
+    chromeOption.add_argument("--lang=zh-CN.UTF8")
+    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
+    driver = webdriver.Chrome(chrome_options=chromeOption)
+    
+    # driver.set_window_size(1024, 960)
+
+    driver.get(f'https://www.google.com.tw/maps/search/{search}/@23.546162,120.6402133,8z?hl=zh-TW/data=!4m4!2m3!5m1!2e1!6e5')
+    
+
+    #driver.maximize_window()
+
+    driver.implicitly_wait(2)
+
+    operation = driver.find_element(By.XPATH, '//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[1]')
+    
+    name_type = operation.find_elements(By.CLASS_NAME, 'Nv2PK')
+    name = [i.text.split('\n')[0] for i in name_type]
+
+
+    driver.quit()
+
+    return name[0]
