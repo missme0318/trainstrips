99849a107ac7cf5c57ef5a76f354a4a2feec25ef
missme0318 <your_email>
2022-07-11 08:13:53 +0800

    test

--- recaptha.py
+++ recaptha.py
@@ -12,18 +12,18 @@ from twocaptcha import TwoCaptcha
 import time
 import os
 
-def solveRecaptha(sitekey, pageurl):
-    api_key = os.getenv('APIKEY_2CAPTCHA', '4bca3ca456af17b4be31f166e1ddb8aa')
+# def solveRecaptha(sitekey, pageurl):
+#     api_key = os.getenv('APIKEY_2CAPTCHA', '4bca3ca456af17b4be31f166e1ddb8aa')
 
-    solver = TwoCaptcha(api_key)
+#     solver = TwoCaptcha(api_key)
 
-    try:
-        result = solver.recaptcha(
-            sitekey=sitekey,
-            url=pageurl)
+#     try:
+#         result = solver.recaptcha(
+#             sitekey=sitekey,
+#             url=pageurl)
 
-    except Exception as e:
-        print(e)
+#     except Exception as e:
+#         print(e)
 
-    else:
-        return result
+#     else:
+#         return result
