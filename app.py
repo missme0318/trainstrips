e963016360a2c42ca63397a8d95d8990b42b4150
missme0318 <your_email>
2022-07-11 08:04:51 +0800

    test

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from selenium.webdriver.support.ui import Select
 
 from googlemap import input_wanted
 from searchtrips import search_trips
-from booking import booking_train, solveRecaptha
-import os
+from booking import solveRecaptha
 
 app = Flask(__name__)
 
--- booking.py
+++ booking.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
