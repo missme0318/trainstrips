55f3a86e8ac324fbfccb2b898fe73497a37fb1f1
missme0318 <your_email>
2022-07-11 14:30:43 +0800

    test

--- googlemap.py
+++ googlemap.py
@@ -5,28 +5,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
-def web_get_address(web):
-    chromeOption = webdriver.ChromeOptions()
-    chromeOption.add_argument("--lang=zh-CN.UTF8")
-    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    driver = webdriver.Chrome(chrome_options=chromeOption)
-
-    # driver.get(web)
-
-    # try:
-    #     address = driver.find_element(By.CLASS_NAME, 'm6QErb .rogA2c').text
-    # except:
-    #     address = '無地址提供'
-        
-    # try:
-    #     time = driver.find_element(By.CLASS_NAME, 'm6QErb .OqCZI').text.split('\n')[0].split('⋅')[0]
-    # except:
-    #     time = '無提供時間'
-    # driver.quit()
-    address = 2
-    time = 3
-    return address, time
-
 def input_wanted(search):
     address, limittime = [], []
   
@@ -49,41 +27,16 @@ def input_wanted(search):
     name = [i.text.split('\n')[0] for i in name_type]
     comment = [i.text.split('\n')[1][:3] for i in name_type]
     website = [str(i.get_attribute('href')) for i in websites]
-    address, time = web_get_address(web[0])
-    # situ = ''
-
-    # try:
-    #     for i in website:
-    #         chromeOption = webdriver.ChromeOptions()
-    #         chromeOption.add_argument("--lang=zh-CN.UTF8")
-    #         chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    #         driver = webdriver.Chrome(chrome_options=chromeOption)
-    #         driver.get(str(i))
-    #         try:
-    #             address = driver.find_element(By.CLASS_NAME, 'm6QErb .rogA2c').text
-    #         except:
-    #             address = '無地址提供'
-    #             situ += '2無地址提供'
-            
-    #         try:
-    #             time = driver.find_element(By.CLASS_NAME, 'm6QErb .OqCZI').text.split('\n')[0].split('⋅')[0]
-    #         except:
-    #             time = '無提供時間'
-    #             situ += '3'
-    #         try:
-    #             address.append(addr)
-    #             limittime.append(time)
-    #         except:
-    #             situ += '4'
-    # except:
-    #     situ += '5'
+    
 
 
-    # mapinfo = ''
-    # for i in zip(name, comment, limittime,address, website):
-    #     mapinfo += f'{i[0]}\n{i[1]}\n{i[2]}\n{i[3]}\n{i[4]}\n\n'
-    # mapinfo = f'{name[0]}\n{comment[0]}\n{website[0]}'
+    mapinfo = ''
+    for i in zip(name, comment, website):
+        mapinfo += f'{i[0]}\n{i[1]}\n{i[2]}\n\n'
+
+    #mapinfo = f'{name[0]}\n{comment[0]}\n{website[0]}'
 
     driver.quit()
 
-    return address
+    return mapinfo
+
