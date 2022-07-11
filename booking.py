0a5aab11d47200c5e7141be8ef14677521d173a0
missme0318 <your_email>
2022-07-11 10:28:52 +0800

    test

--- booking.py
+++ booking.py
@@ -52,22 +52,21 @@ def booking_train(bookinfo):
         
     idlocate = driver.find_element(By.XPATH, '//*[@id="pid"]')
     idlocate.send_keys(IDnum)
-    try:  
-        start = driver.find_element(By.ID, 'startStation')
-        start.send_keys(startwords)
+ 
+    start = driver.find_element(By.ID, 'startStation')
+    start.send_keys(startwords)
 
-        end = driver.find_element(By.ID, 'endStation')
-        end.send_keys(endwords)
+    end = driver.find_element(By.ID, 'endStation')
+    end.send_keys(endwords)
 
+    date = driver.find_element(By.ID,'rideDate1')
+    date.clear()
+    date.send_keys(ridedatebook)
 
-        date = driver.find_element(By.ID,'rideDate1')
-        date.clear()
-        date.send_keys(ridedatebook)
+    trips = driver.find_element(By.ID, 'trainNoList1')
+    trips.send_keys(tripsnums)
 
-
-        trips = driver.find_element(By.ID, 'trainNoList1')
-        trips.send_keys(tripsnums)
-
+    try: 
         driver.find_element(By.ID, 'g-recaptcha-response')
         driver.execute_script(
             "document.getElementById('g-recaptcha-response').innerHTML = '" + code + "'")
