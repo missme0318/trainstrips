b7c82ba9b969a500f6bf9ccd9520e33b45c81f51
missme0318 <your_email>
2022-07-11 08:23:01 +0800

    test

--- recaptha.py
+++ recaptha.py
@@ -1,15 +1,18 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from PIL import Image
-from twocaptcha import TwoCaptcha
 
-import time
-import os
+
+
+
+# from selenium.webdriver.support.ui import WebDriverWait
+# from PIL import Image
+# from twocaptcha import TwoCaptcha
+
+# import os
 
 # def solveRecaptha(sitekey, pageurl):
 #     api_key = os.getenv('APIKEY_2CAPTCHA', '4bca3ca456af17b4be31f166e1ddb8aa')
