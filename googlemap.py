74b39562bc1198f379acab0139a61006572c93b1
missme0318 <your_email>
2022-07-11 15:49:32 +0800

    test

--- googlemap.py
+++ googlemap.py
@@ -5,25 +5,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
-def web_get_address(web):
-    chromeOption = webdriver.ChromeOptions()
-    chromeOption.add_argument("--lang=zh-CN.UTF8")
-    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    driver = webdriver.Chrome(chrome_options=chromeOption)
+# def web_get_address(web):
+#     chromeOption = webdriver.ChromeOptions()
+#     chromeOption.add_argument("--lang=zh-CN.UTF8")
+#     chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
+#     driver = webdriver.Chrome(chrome_options=chromeOption)
     
-    driver.get(web)
+#     driver.get(web)
 
-    try:
-        address = driver.find_element(By.CLASS_NAME, 'm6QErb .rogA2c').text
-    except:
-        address = '無地址提供'
+#     try:
+#         address = driver.find_element(By.CLASS_NAME, 'm6QErb .rogA2c').text
+#     except:
+#         address = '無地址提供'
         
-    try:
-        time = driver.find_element(By.CLASS_NAME, 'm6QErb .OqCZI').text.split('\n')[0].split('⋅')[0]
-    except:
-        time = '無提供時間'
-    driver.quit()
-    return address, time
+#     try:
+#         time = driver.find_element(By.CLASS_NAME, 'm6QErb .OqCZI').text.split('\n')[0].split('⋅')[0]
+#     except:
+#         time = '無提供時間'
+#     driver.quit()
+#     return address, time
 
 def input_wanted(search):
     address, limittime = [], []
