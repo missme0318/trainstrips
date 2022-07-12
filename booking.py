5e32c163af4039636573f403abe92c9f586a8cba
missme0318 <your_email>
2022-07-12 12:38:01 +0800

    test

--- booking.py
+++ booking.py
@@ -3,14 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
-#from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support.ui import 
 from selenium.webdriver.support import expected_conditions as EC
+from twocaptcha import TwoCaptcha
 
 import time
+import os
+import base64
+
     
+
 # 訂票
 
-def booking_train(code, bookinfo):
+def booking_train(bookinfo):
 
     IDnum = bookinfo.split('\n')[0]
     startwords = bookinfo.split('\n')[1]
