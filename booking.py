054f7b0a2e120dbf5d7633a6a8da9432d5b38d0e
missme0318 <your_email>
2022-07-12 10:40:39 +0800

    test

--- booking.py
+++ booking.py
@@ -68,7 +68,7 @@ def booking_train(bookinfo):
     driver.find_element(By.ID, 'g-recaptcha-response')
     driver.execute_script("document.getElementById('g-recaptcha-response').innerHTML = '" + code + "'")
 
-    # time.sleep(2)
+    time.sleep(3)
     
     driver.find_element(By.XPATH, '//*[@id="queryForm"]/div[4]/input[2]').click()
 
