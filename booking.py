a0daf828808a36d3500f80738476877ea8454158
missme0318 <your_email>
2022-07-12 14:14:44 +0800

    test

--- booking.py
+++ booking.py
@@ -6,11 +6,28 @@ from selenium.webdriver.support.ui import Select
 # from selenium.webdriver.support.ui import 
 from selenium.webdriver.support import expected_conditions as EC
 from twocaptcha import TwoCaptcha
-from recaptha import solveRecaptha
 
 import time
 import os
 import base64
+
+def solveRecaptha():
+    train_booking_pageurl = 'https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip121/bookingTicket'
+    train_booking_sitekey = '6LdHYnAcAAAAAI26IgbIFgC-gJr-zKcQqP1ineoz'
+
+    api_key = os.getenv('APIKEY_2CAPTCHA', '4bca3ca456af17b4be31f166e1ddb8aa')
+
+    solver = TwoCaptcha(api_key)
+
+    try:
+        result = solver.recaptcha(sitekey=train_booking_sitekey,url=train_booking_pageurl)
+
+    except Exception as e:
+        print(e)
+
+    else:
+        code = result['code']
+        return code
 
     
 
