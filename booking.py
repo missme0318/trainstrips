ed3c6dccf29fb23a48a0adde3f3674414940ee35
missme0318 <your_email>
2022-07-13 06:46:22 +0800

    test

--- booking.py
+++ booking.py
@@ -34,20 +34,20 @@ def solveRecaptha(bookinfo):
 
 
 def booking_train(code, bookinfo):
-
-    IDnum = bookinfo.split('\n')[0]
-    startwords = bookinfo.split('\n')[1]
-    endwords = bookinfo.split('\n')[2]
-    ridedatebook = bookinfo.split('\n')[3]
-    tripsnums = bookinfo.split('\n')[4]
-
-    chromeOption = webdriver.ChromeOptions()
-    chromeOption.add_argument("--lang=zh-CN.UTF8")
-    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    driver = webdriver.Chrome(chrome_options=chromeOption)
-    
-    driver.get('https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip121/query')
     try: 
+        IDnum = bookinfo.split('\n')[0]
+        startwords = bookinfo.split('\n')[1]
+        endwords = bookinfo.split('\n')[2]
+        ridedatebook = bookinfo.split('\n')[3]
+        tripsnums = bookinfo.split('\n')[4]
+
+        chromeOption = webdriver.ChromeOptions()
+        chromeOption.add_argument("--lang=zh-CN.UTF8")
+        chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
+        driver = webdriver.Chrome(chrome_options=chromeOption)
+        
+        driver.get('https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip121/query')
+    
         idlocate = driver.find_element(By.XPATH, '//*[@id="pid"]')
         idlocate.send_keys(IDnum)
 
