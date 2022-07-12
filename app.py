3ac5b51b0e09ec9d34b24455c77ab4c53f838a6c
missme0318 <your_email>
2022-07-12 12:58:37 +0800

    test

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def handle_message(event):
     elif '訂購\n' in msg:
         bookinfo = msg.replace('訂購\n','')
         code = solveRecaptha()
-        r = booking_train(code, bookinfo)
+        r = booking_train(str(code), bookinfo)
     elif '取消車票' in msg:
         r = '請依格式輸入ID/車次\n例如：\n\n取消\nSP223210741\n554'
     elif '取消\n' in msg:
