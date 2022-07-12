ea3cd023f8141b8a64d1442ad69910093e767461
missme0318 <your_email>
2022-07-12 12:43:04 +0800

    test

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
 from searchtrips import search_trips
-from booking import booking_train, solveRecaptha
+# from booking import booking_train, solveRecaptha
 from foods import delicious
 from cancel import cancel_train
 # from recaptha import solveRecaptha
