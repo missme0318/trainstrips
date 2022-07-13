9049241c4c8d618dea8eab0219b9d1c6903d3782
missme0318 <your_email>
2022-07-13 10:20:44 +0800

    test

--- booking2.py
+++ booking2.py
@@ -57,15 +57,16 @@ def booking_train2(code, bookinfo):
     date.clear()
     date.send_keys(ridedatebook)
     driver.find_element(By.ID, 'trainNoList1').send_keys(tripsnums)
-    try:
-
-        driver.find_element(By.ID, 'g-recaptcha-response')
-        driver.execute_script("document.getElementById('g-recaptcha-response').innerHTML = '" + code + "'")
-
-        time.sleep(2)
-
-        driver.find_element(By.XPATH, '//*[@id="queryForm"]/div[4]/input[2]').click()
+    
+    driver.find_element(By.ID, 'g-recaptcha-response')
+    driver.execute_script("document.getElementById('g-recaptcha-response').innerHTML = '" + code + "'")
+    
+    time.sleep(2)
+    
         
+    try:
+        driver.find_element(By.XPATH, '//*[@id="queryForm"]/div[4]/input[2]').click()
+    
         time.sleep(2)
         
         try:
