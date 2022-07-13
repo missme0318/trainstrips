d1749d2f3bfb92c945c66688959fdc28e582d93f
missme0318 <your_email>
2022-07-13 11:29:57 +0800

    test

--- cancel.py
+++ cancel.py
@@ -5,52 +5,56 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 import re
-
+import sys
 
 def cancel_train(cancelinfo):
 
-    IDnum = cancelinfo.split('\n')[0]
+    idnum = cancelinfo.split('\n')[0]
     ticketnum = cancelinfo.split('\n')[1]
 
-    chromeOption = webdriver.ChromeOptions()
-    chromeOption.add_argument("--lang=zh-CN.UTF8")
-    chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
-    driver = webdriver.Chrome(chrome_options=chromeOption)
+    idcorrect = re.fullmatch(r'[A-Za-z](\d{9})',idnum).group()
+    ticketcorrect = re.fullmatch(r'(20\d{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])',sourceDate).group()
 
     try:
-        re.fullmatch(r'[A-Za-z](\d{9})',IDnum)
-    except:
-        finish_text = '身分證字號格式錯誤\n請重新輸入'
-        break
-  
-    driver.get('https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip115/query')
+        idcorrect == idnum
+        ticketcorrect == ticketnum
 
-    cancelID = driver.find_element(By.ID, 'pid')
-    cancelID.send_keys(IDnum)
+        chromeOption = webdriver.ChromeOptions()
+        chromeOption.add_argument("--lang=zh-CN.UTF8")
+        chromeOption.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0')
+        driver = webdriver.Chrome(chrome_options=chromeOption)
+      
+        driver.get('https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip115/query')
 
-    cancelticketnum = driver.find_element(By.ID, 'bookingcode')
-    cancelticketnum.send_keys(ticketnum)
+        cancelID = driver.find_element(By.ID, 'pid')
+        cancelID.send_keys(IDnum)
 
-    checkout = driver.find_element(By.XPATH, '//*[@id="queryForm"]/div[7]/button').click()
-    try:
-        cancelclick = driver.find_element(By.ID, 'cancel').click()
+        cancelticketnum = driver.find_element(By.ID, 'bookingcode')
+        cancelticketnum.send_keys(ticketnum)
 
-        cancelclick2 = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div/div/div/div/div/div/div[4]/button[2]').click()
+        checkout = driver.find_element(By.XPATH, '//*[@id="queryForm"]/div[7]/button').click()
+        try:
+            cancelclick = driver.find_element(By.ID, 'cancel').click()
 
-        driver.get_screenshot_as_file('static/cancel_finish.png')
+            cancelclick2 = driver.find_element(By.XPATH, '/html/body/div[6]/div[2]/div/div/div/div/div/div/div/div[4]/button[2]').click()
 
-        cancelfinish1 = driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/dl/dd').text
-        cancelfinish1 = cancelfinish1.replace('已取消 ', '')
-        cancelfinish2 = driver.find_element(By.XPATH, '//*[@id="content"]/div[5]/div/div[1]/div[1]').text
+            driver.get_screenshot_as_file('static/cancel_finish.png')
+
+            cancelfinish1 = driver.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/dl/dd').text
+            cancelfinish1 = cancelfinish1.replace('已取消 ', '')
+            cancelfinish2 = driver.find_element(By.XPATH, '//*[@id="content"]/div[5]/div/div[1]/div[1]').text
+            
+            finish_text = f'{cancelfinish2}\n{cancelfinish1}'
+        except:
+            finishcancel = driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/div[1]/dl/dd').text
+            if '已取消' in finishcancel:
+                finish_text = finishcancel
+            driver.get_screenshot_as_file('static/cancel_finish.png')
+
+        driver.delete_all_cookies()
+        driver.quit()
         
-        finish_text = f'{cancelfinish2}\n{cancelfinish1}'
     except:
-        finishcancel = driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/div[1]/dl/dd').text
-        if '已取消' in finishcancel:
-            finish_text = finishcancel
-        driver.get_screenshot_as_file('static/cancel_finish.png')
-
-    driver.delete_all_cookies()
-    driver.quit()
+        finish_text = '格式錯誤，請重新輸入\n注意！票號為7碼'
 
     return finish_text
